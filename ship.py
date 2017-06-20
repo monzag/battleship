09d5556ab9674f1d63e6fd56c15877f43d318b48
monzag <zaganiacz.m@gmail.com>
2017-06-20 11:38:28 +0200

    Delete useless file

--- ship.py
+++
@@ -1,9 +0,0 @@
-class Ship:
-    
-    def __init__(self, column, row, size, is_horizontal):
-        self.column = column
-        self.row = row
-        self.size = size
-        self.is_horizontal = is_horizontal
-
-        self.squares = []
