be842090f652cb8d2de6148cbe1d5d9db86b889b
monzag <zaganiacz.m@gmail.com>
2017-06-20 11:02:02 +0200

    Add create_board

--- specifications/ocean.py
+++ specifications/ocean.py
@@ -1,6 +1,13 @@
+# import Square
+
+
 class Ocean():
 
     def __init__(self):
 
         self.board = []
+
+    def create_board(self):
+        return [[Square(row, column) for column in range(10)] for row in range(10)]
+
 
