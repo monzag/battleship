0f0c3494d895a19de8c514483f839c360ebfb083
monzag <zaganiacz.m@gmail.com>
2017-06-20 11:31:03 +0200

    Add init

--- model/ship.py
+++ model/ship.py
@@ -1,2 +1,11 @@
+class Ship:
 
+    def __init__(self, row, column, size, is_vertical=True):
+
+        self.row = row
+        self.column = column
+        self.size = size
+        self.squares = []
+        self.is_sunk = False
+        self.is_vertical = is_vertical
 
