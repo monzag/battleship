579b8d69f237d652143d34e680737cd1aa0ed7c9
monzag <zaganiacz.m@gmail.com>
2017-06-20 15:12:17 +0200

    Add init, check_sunk, insert_ship_to_ocean

--- model/ship.py
+++ model/ship.py
@@ -1,3 +1,7 @@
+from square import Square
+from ocean import Ocean
+
+
 class Ship:
 
     def __init__(self, row, column, size, is_vertical=True):
@@ -12,4 +16,23 @@ class Ship:
     def check_sunk(self):
         if not self.is_sunk:
             self.is_sunk = True
+
+    def insert_ship_to_ocean(self, ocean, start_row, start_column, size):
+        for i in range(self.size):
+            if self.is_vertical:
+                ocean.board[start_row + i][start_column].set_as_ship()
+            else:
+                ocean.board[start_row][start_column + i].set_as_ship()
+
+        return ocean
+
+    def check_free_position(self):
+        for i in range(self.size):
+            if self.is_vertical:
+                if not ocean.board[start_row + i][start_column].is_ship():
+                    pass
+
+            else:
+                if not ocean.board[start_row][start_column + i].is_ship():
+                    pass
 
