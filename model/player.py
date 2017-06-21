693f8efcacb1f3583eb8c9a6933da873a55909e9
monzag <zaganiacz.m@gmail.com>
2017-06-21 15:09:41 +0200

    Add get ships from player

--- model/player.py
+++ model/player.py
@@ -10,27 +10,30 @@ class Player:
         self.ocean = ocean
         self.is_fisrt = True
 
-    def set_ship(self, row, column, is_vertical):
+    def set_ship(self, row, column, is_vertical, size):
         ship = Ship(size, is_vertical)
-        if ship.can_be_set(ocean, row, column):
-            ship.insert_ship_to_ocean(ocean, row, column)
+        if ship.can_be_set(self.ocean, row, column):
+            ship.insert_ship_to_ocean(self.ocean, row, column)
             self.ships.append(ship)
 
     def is_input_valid(self, pos_x, pos_y, letter, is_vertical):
-        if pos_x.isdigit() and pos_y in list(letter) and is_vertical.upper() in ['Y', 'N']:
+        if pos_x.isdigit() and pos_y in list(letter) and is_vertical in ['Y', 'N']:
 
-            if 0 < pos_x < 10:
+            if 0 < int(pos_x) < 10:
                 return True
 
         return False
 
     def get_positions_from_player(self, size):
         letter = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
+        pos_x = ''
+        pos_y = ''
+        is_vertical = ''
 
-        while not self.is_input_valid(pos_x, pos_y, letter):
-            pos_y = input('Write position y (A-J): ')
+        while not self.is_input_valid(pos_x, pos_y, letter, is_vertical):
+            pos_y = input('Write position y (A-J): ').upper()
             pos_x = input('Write position x (1-10): ')
-            is_vertical = input('Should your ship be vertical? (y/n): ')
+            is_vertical = input('Should your ship be vertical? (y/n): ').upper()
 
         if is_vertical.upper() == 'Y':
             is_vertical = True
@@ -40,7 +43,7 @@ class Player:
         column = int(pos_x) - 1
         row = letter[pos_y]
 
-        self.set_ship(row, column, is_vertical)
+        self.set_ship(row, column, is_vertical, size)
 
     def is_game_win(self):
         for ship in self.ships:
@@ -53,6 +56,13 @@ class Player:
     def get_ships_from_player(self):
         for size in range(1, 6):
             self.get_positions_from_player(size)
+
+ocean = Ocean()
+player = Player('Arek', ocean)
+print(ocean.get_ocean_string(True))
+
+player.get_ships_from_player()
+print(ocean.get_ocean_string(True))
 
 
 
