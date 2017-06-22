b710eca6ce19cac1c35ac6f079d6e569a563b38b
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-06-22 11:25:41 +0200

    Deleted unneccesery __str__ method

--- model/ocean.py
+++ model/ocean.py
@@ -1,4 +1,6 @@
 from square import Square
+from colors import Colors
+from output_menager import OutputMenager
 
 
 class Ocean:
@@ -6,20 +8,16 @@ class Ocean:
     _ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
     def __init__(self):
-
         self.board = self.create_board()
 
     def create_board(self):
         return [[Square(row, column) for column in range(10)] for row in range(10)]
 
     def get_ocean_string(self, is_player_own):
-        board_str = '   ' + ' '.join(list(str(i) for i in range(len(self.board))))
+        board_str = Colors.OKGREEN + '  ' + ' '.join(list(str(i) for i in range(len(self.board)))) + Colors.ENDC
 
         for i, row in enumerate(self.board):
             line = ' '.join(squere.get_squere_string(is_player_own) for squere in row)
-            board_str += '\n{} {}'.format(self._ROWS[i], line)
+            board_str += '\n' + Colors.OKGREEN + self._ROWS[i] + Colors.ENDC + ' ' + line
 
         return board_str
-
-    def __str__(self):
-        return '\n'.join('  '.join([str(square) for square in row]) for row in self.board)
