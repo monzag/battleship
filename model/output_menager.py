6edc3f897e6c25f656520ea2ddf4c81f92dfce04
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-06-22 11:26:43 +0200

    Changed parameters of func in output_menager from strings to ocean obj.

--- model/output_menager.py
+++ model/output_menager.py
@@ -3,30 +3,48 @@ from colors import Colors
 
 
 class OutputMenager:
-    
+
+    _ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
     @classmethod
-    def print_battlefild(cls, own_ocean, enemy_ocean):
+    def print_battlefield(cls, own_ocean, enemy_ocean):
         '''
         Prints battlefild(ocean obj. of both players) next to each other,
         every next turn
 
         Parameters:
-            own_battlefield   : ocean as string (current player that has turn)
-            enemy_battlefiels : ocean as string (other player)
+            own_battlefield   : ocean obj (current player that has turn)
+            enemy_battlefiels : ocean obj (other player)
         
         Return
             None
         '''
-        own_ocean = own_ocean.split('\n')
-        enemy_ocean = enemy_ocean.split('\n')
+        own_ocean = own_ocean.get_ocean_string(True).split('\n')
+        enemy_ocean = enemy_ocean.get_ocean_string(False).split('\n')
 
         if len(own_ocean) != len(enemy_ocean):
             raise ValueError
-        
+
+        battlefield = ['{}    {}'.format(own_ocean[i], enemy_ocean[i]) for i in range(len(own_ocean))]
+        battlefield = '\n'.join(battlefield)
+
         os.system('clear')
-        battlefild = '\n'.join([' {}    {}'.format(own_ocean[i], enemy_ocean[i]) for i in range(len(own_ocean))])
-        battlefild += '\n' + Colors.OKGREEN +'{: ^24}  '.format('YOUR MAP')
-        battlefild += '{: ^24}\n'.format('ENEMY MAP') + Colors.ENDC
+        print(battlefield)
+    
+    @classmethod
+    def print_single_battlefield(cls, ocean, is_player_own):
+        '''
+        Prints single ocean which is player owns
+
+        Parameters:
+            ocean : Ocean obj
+        
+        Return:
+            None
+        '''
+        os.system('clear')
+        battlefild = ocean.get_ocean_string(is_player_own)
+        
         print(battlefild)
 
         
