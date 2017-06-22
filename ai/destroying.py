3ec68986dbe21d9fd1487683b0a88cab5e168ce7
Cezary Broś <cezary.bros+git@gmail.com>
2017-06-22 02:11:06 +0200

    Rewrite class constructor

--- ai/destroying.py
+++ ai/destroying.py
@@ -3,9 +3,11 @@ from random import random
 
 class Destroying:
 
-    def __init__(self):
-        self.guess_hit = False
-        self.first_guess = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    def __init__(self, row, col):
+        self.initial_position = (row, col)
+        self.direction_to_check = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        self.direction = self._choose_direction()
+        self.current_position = self.initial_position
 
     def guess_pursue_direction(self, hit_row, hit_col):
         guess = self.first_guess.pop(random(len(self.first_guess)))
