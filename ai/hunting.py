dd4626830508913334b35a69d7dc32a2af549689
Cezary Broś <cezary.bros+git@gmail.com>
2017-06-22 10:25:38 +0200

    Simplify Hunting class

--- ai/hunting.py
+++ ai/hunting.py
@@ -3,16 +3,11 @@ from random import randint
 
 class Hunting:
 
-    def __init__(self):
-        self.used = []
-
-    def shoot_random(self):
+    def shoot_random(self, shots):
         random_row = randint(0, 9)
         random_col = randint(0, 9)
         # prevent from shooting the same spot again
-        while (random_row, random_col) in self.used:
+        while (random_row, random_col) in shots:
             random_row = randint(0, 9)
             random_col = randint(0, 9)
-
-        self.used.append((random_row, random_col))
         return random_row, random_col
